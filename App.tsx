2c822e8e22c8561f291b902735d98a40df3b9263
Alex Kharouk <akharoukpp@gmail.com>
2020-04-05 18:36:42 +0100

    set up material ui although it might not work

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from '@material-ui/core';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Planted</Text>
+      {/* <Button title="Log In" onPress={() => {}}>Log In</Button> */}
+      <Button><Text></Text></Button>
     </View>
   );
 }
@@ -14,6 +17,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: "30%"
   },
+
+  title: {
+    fontSize: 32
+  }
 });
